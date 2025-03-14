45ad7d71011c7d8e86616115b5d73bf6441402b9
keethu-jay <keerthana.sa.jayamoorthy@gmail.com>
2025-03-14 14:17:29 -0400

    add new pages and background pics

---
+++ portfolio-site/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Button, Typography } from '@mui/material';
+import {useHistory, useNavigate} from 'react-router-dom';
+import './App.css'; // Import your CSS
+import backgroundImage from '../Assets/home_background.png'; // Correct path
+
+function App() {
+    const navigate = useNavigate();
+
+    const handlePortfolioRedirect = () => {
+        navigate('/Projects'); // Update this to your PortfolioPage route
+    };
+
+    return (
+        <div
+            className="app-background"
+            style={{ backgroundImage: `url(${backgroundImage})` }} // Use inline style
+        >
+            <div className="app-content"> {/* Scoped class for content */}
+                <Typography variant="h2" component="h1" className="name">
+                    Your Name
+                </Typography>
+                <Typography variant="h5" component="h2" className="title">
+                    Digital Engineer
+                </Typography>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handlePortfolioRedirect}
+                    className="portfolio-button"
+                >
+                    View Portfolio
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ portfolio-site/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Contact from './ContactPage';
+import Projects from './ProjectsPage';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/about" element={<Contact />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>
+);
+
+reportWebVitals();
