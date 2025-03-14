1d945763eca3b110ff910a503261c6e288bb2ecf
keethu-jay <keerthana.sa.jayamoorthy@gmail.com>
2025-03-14 14:22:15 -0400

    add new pages and background pics

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Typography } from '@mui/material';
 import {useHistory, useNavigate} from 'react-router-dom';
 import './App.css'; // Import your CSS
-import backgroundImage from '../Assets/home_background.png'; // Correct path
+import backgroundImage from '../assets/home_background.png'; // Correct path
 
 function App() {
     const navigate = useNavigate();
