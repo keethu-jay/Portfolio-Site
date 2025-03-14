681e7edfce75371ca517c659f7cd8873b8751804
keethu-jay <keerthana.sa.jayamoorthy@gmail.com>
2025-03-14 14:11:20 -0400

    add new pages and background pics

---
+++ portfolio-site/src/ContactPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function ContactPage() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the About page content.</p>
+        </div>
+    );
+}
+
+export default ContactPage;
---
+++ portfolio-site/src/ProjectsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProjectPage() {
+    return (
+        <div>
+            <h1>Projects</h1>
+            <p>This is the Projects page content.</p>
+        </div>
+    );
+}
+
+export default ProjectPage;
